3f169e2196036ff1d71d496aabdfd8600b944dad
Ajay Mali <ajaymali2701@gmail.com>
2024-04-14 20:14:04 +0530

    Lowercase to Uppercase

---
+++ codes/src/J_11_LowercaseToUppercase/L2U.java
@@ -0,0 +1,23 @@
+package J_11_LowercaseToUppercase;
+
+public class L2U {
+
+    static String toLower(String s) {
+
+        return 
+    }
+    public static void main(String[] args) {
+
+        String output = "";
+        String name = "ABCddE";
+        int str = name.length();
+
+        for (int i = 0; i < str; i++) {
+            output = name.toLowerCase();
+
+        }
+        System.out.println(output);
+
+
+    }
+    }
